ff1c3a539ccdd0cc17cb45c06fb2f0b4e5f3b83e
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-06 19:59:07 +0100

    console col tablazat

---
+++ colspanjs/index.js
@@ -0,0 +1,6 @@
+console.log('Szerző        | Mű                                | Fogalmak |          | ')  //kíírjuk a táblázat headjet ahol van egy üres cella
+console.log('Apollinaire   | A megsebzett galamb és a szökőkút | képvers  | emlékezés| ')  //kíírjuk a táblázat legelső adatsorát anu teljes
+console.log('Apollinaire   | Búcsú                             | avantgárd|          | ')  //kíírjuk a táblázat 2. adatsorát ahol van üres cella
+console.log('Thomas Mann   | Mario és a varázsló               | kisregény|          | ')  //kíírjuk a táblázat 3. adatsorát ahol van üres cella
+console.log('Franz Kafka   | A per                             | regény   |          | ')  //kíírjuk a táblázat 4. adatsorát ahol van üres cella
+console.log('Franz Kafka   | Az átváltozás                     | kisregény| groteszk | ')  //kíírjuk a táblázat 5. adatsorát ami teljes
