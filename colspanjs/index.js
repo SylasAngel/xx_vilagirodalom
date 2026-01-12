dc5138cbf8ad93575c1257a5cffafddbfbbaf7ed
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-12 15:29:44 +0100

    console col elagazas

--- colspanjs/index.js
+++ colspanjs/index.js
@@ -1,44 +1,113 @@
-/**
- * @type {string []} szöveg tömb fejléc elemei
- */
-const header = ['Szerző','Mű','Fogalmak'] //fejléc elemei
+
+
 
 /**
- * @type {{Author:string,Creation:string,FirstConcept:string,SecondConcept?:string} []} adatsorok elemei
+ * @type {string []} header elemei
  */
-const DataArray = [ //adattömb
+const header = ['Szerző','Mű','Fogalmak'] //header elemei
+
+/**
+ * @type {{Author:string,Creation:string,FirstConcept:string,SecondConcept?:string}[]} Array elemei, ahol SecondConcept nem biztos hogy van
+ */
+const DataArray = [ //Array
     {
-        Author: 'Appollinaire', //adatsor szerzője
-        Creation: 'A megsebzett galamb és a szökőkút', //adatsor műve
-        FirstConcept: 'képvers', //adatsor első fogalma
-        SecondConcept: 'emlékezés' //adatsor második fogalma
+        Author:'Apollinaire', //Szerző
+        Creation:'A megsebzett galamb és a szökőkút', //Mű
+        FirstConcept: 'képvers', //Első fogalom
+        SecondConcept: 'emlékezés' //második fogalom
     },
     {
-        Author: 'Apollinaire',//adatsor szerzője
-        Creation: 'Búcsú',//adatsor műve
-        FirstConcept: 'avantgárd',//adatsor első fogalma
+        Author:  'Apollinaire', //Szerző
+        Creation:'Búcsú', //Mű
+        FirstConcept: 'avantgárd' //Első fogalom
     },
     {
-        Author: 'Thomas Mann',//adatsor szerzője
-        Creation: 'Mario és a varázsló',//adatsor műve
-        FirstConcept: 'kisregény',//adatsor első fogalma
+        Author:  'Thomas Mann', //Szerző
+        Creation:'Mario és a varázsló', //Mű
+        FirstConcept: 'kisregény' //Első fogalom
     },
     {
-        Author: 'Franz Kafka',//adatsor szerzője
-        Creation: 'A per',//adatsor műve
-        FirstConcept: 'regény',//adatsor első fogalma
+        Author: 'Franz Kafka', //Szerző
+        Creation: 'A per', //Mű
+        FirstConcept: 'regény' //Első fogalom
     },
     {
-        Author: 'Franz Kafka',//adatsor szerzője
-        Creation: 'Az átvlátozás',//adatsor műve
-        FirstConcept: 'kisregény',//adatsor első fogalma
-        SecondConcept: 'groteszk'//adatsor második fogalma
+        Author:'Franz Kafka', //Szerző
+        Creation:'Az átváltozás', //Mű
+        FirstConcept: 'kisregény', //Első fogalom
+        SecondConcept: 'groteszk' //második fogalom
     }
 ]
+/**
+ * @type {string} első sor
+ */
+let FirstRow = DataArray[0].Author+'|'+ DataArray[0].Creation+'|'+DataArray[0].FirstConcept+'|' //Első sor 
+if(DataArray[0].SecondConcept != undefined) //leellenőrizzük hogy a secondConcept nem undefined-e
+{
+    FirstRow+= DataArray[0].SecondConcept+ '|' //hozzáadjuk az első sorhoz a második fogalmat, igaz ág
+}
+else //else ág, hamis ág, ha a SecondConceptbe nincs semmi
+{
+    FirstRow+= '|' // hozzáfűzzük az első sorhoz az üres mezőt
+}
 
-console.log(header[0],'|',header[1],'|',header[2],'|','|') //fejléc kiíratása
-console.log(DataArray[0].Author,'|',DataArray[0].Creation,'|',DataArray[0].FirstConcept,'|',DataArray[0].SecondConcept,'|') //első adatsor kiíratása
-console.log(DataArray[1].Author,'|',DataArray[1].Creation,'|',DataArray[1].FirstConcept,'|','|') //második adatsor kiíratása
-console.log(DataArray[2].Author,'|',DataArray[2].Creation,'|',DataArray[2].FirstConcept,'|','|') //harmadik adatsor kiíratása
-console.log(DataArray[3].Author,'|',DataArray[3].Creation,'|',DataArray[3].FirstConcept,'|','|') //negyedik adatsor kiíratása
-console.log(DataArray[4].Author,'|',DataArray[4].Creation,'|',DataArray[4].FirstConcept,'|',DataArray[4].SecondConcept,'|') //ötödik adatsor kiíratása
+/**
+ * @type {string} második sor
+ */
+let SecondRow = DataArray[1].Author+'|'+ DataArray[1].Creation+'|'+DataArray[1].FirstConcept+'|' //leellenőrizzük hogy a secondConcept nem undefined, e
+if(DataArray[1].SecondConcept != undefined) //leellenőrizzük hogy a secondConcept nem undefined-e
+{
+    FirstRow+= DataArray[1].SecondConcept+'|'//hozzáadjuk az első sorhoz a második fogalmat, igaz ág
+
+}
+else //else ág, hamis ág, ha a SecondConceptbe nincs semmi
+{
+    SecondRow+= '|' // hozzáfűzzük a második sorhoz az üres mezőt
+}
+
+/**
+ * @type {string} harmadik sor
+ */
+let ThirdRow = DataArray[2].Author+'|'+ DataArray[2].Creation+'|'+DataArray[2].FirstConcept+'|' //leellenőrizzük hogy a secondConcept nem undefined, e
+if(DataArray[2].SecondConcept != undefined) //leellenőrizzük hogy a secondConcept nem undefined-e
+{
+    ThirdRowRow+= DataArray[2].SecondConcept+'|' //hozzáadjuk az első sorhoz a második fogalmat, igaz ág
+
+}
+else //else ág, hamis ág, ha a SecondConceptbe nincs semmi
+{
+    ThirdRow+= '|' // hozzáfűzzük a harmadik sorhoz az üres mezőt
+}
+
+/**
+ * @type {string} negyedik sor
+ */
+let FourthRow = DataArray[3].Author+'|'+ DataArray[3].Creation+'|'+DataArray[3].FirstConcept+'|' //leellenőrizzük hogy a secondConcept nem undefined, e
+if(DataArray[3].SecondConcept != undefined) //leellenőrizzük hogy a secondConcept nem undefined-e
+{
+    FourthRow= DataArray[3].SecondConcept+'|' //hozzáadjuk az első sorhoz a második fogalmat, igaz ág
+}
+else //else ág, hamis ág, ha a SecondConceptbe nincs semmi
+{
+  FourthRow+= '|' // hozzáfűzzük a negyedik sorhoz az üres mezőt
+}
+
+/**
+ * @type {string} ötödik sor
+ */ 
+let FifthRow = DataArray[4].Author+'|'+ DataArray[4].Creation+'|'+DataArray[4].FirstConcept+'|' //leellenőrizzük hogy a secondConcept nem undefined, e
+if(DataArray[4].SecondConcept != undefined) //leellenőrizzük hogy a secondConcept nem undefined-e
+{
+    FifthRow+= DataArray[4].SecondConcept+'|' //hozzáadjuk az első sorhoz a második fogalmat, igaz ág
+}
+else //else ág, hamis ág, ha a SecondConceptbe nincs semmi
+{
+    FifthRow+= '|' // hozzáfűzzük az ötödik sorhoz az üres mezőt
+}
+
+console.log(header[0]+'|'+header[1]+'|'+header[2]+'|'+'|') //kiíratjuk a headert
+console.log(FirstRow) //kiratjuk az első sort
+console.log(SecondRow) //kiíratjuk a második sort
+console.log(ThirdRow) //kiíratjuk a harmadik sort
+console.log(FourthRow) //kiíratjuk a negyedik sort
+console.log(FifthRow) //kiíratjuk az ötödik sort
