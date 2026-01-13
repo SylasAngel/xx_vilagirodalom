7dd2ab9ab51ccff6101adda30b60d46bbc4c6f82
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-13 22:27:07 +0100

    console row valtozo bev

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,6 +1,78 @@
-console.log('Szerző        | Mű                                | Fogalmak  | ')  //kíírjuk a táblázat headjet 
-console.log('Apollinaire   | A megsebzett galamb és a szökőkút | képvers   | ')  //kíírjuk a táblázat legelső adatsorát 
-console.log('Apollinaire   | Búcsú                             | avantgárd | ')  //kíírjuk a táblázat 2. adatsorát 
-console.log('Thomas Mann   | Mario és a varázsló               | kisregény | ')  //kíírjuk a táblázat 3. adatsorát 
-console.log('Franz Kafka   | A per                             | regény    | ')  //kíírjuk a táblázat 4. adatsorát 
-console.log('Franz Kafka   | Az átváltozás                     | kisregény | ')  //kíírjuk a táblázat 5. adatsorát 
+/**
+ * @type {string} Head legelső eleme
+ */
+const headAuthor = 'Szerző' //head legelső elemét eltároljuk egy változóba
+/**
+ * @type {string} Head második eleme
+ */
+const headCreation =  'Mű ' //Head második elemét eltároljuk egy változóba
+/**
+ * @type {string} Head harmadik eleme
+ */
+const headConcept = 'Fogalmak' //head harmadik elemét eltároljuk egy változóba
+
+/**
+ * @type {string} első sor 1. eleme
+ */
+const ftdAuthor = 'Apollinaire ' //első és második sor 1. elemét eltároljuk egy változóba
+/**
+ * @type {string} első sor 2. eleme
+ */
+const ftdCreation = 'A megsebzett galamb és a szökőkút' //első sor 2. elemét eltároljuk egy változóba
+/**
+ * @type {string} első sor 3. eleme
+ */
+const ftdFirstConcept = 'képvers ' //első sor 3. elemét eltároljuk egy változóba
+
+
+/**
+ * @type {string} második sor 2. eleme
+ */
+const stdCreation = 'Búcsú ' //második sor 2. elemét eltároljuk egy változóba
+/**
+ * @type {string} második sor 3. eleme
+ */
+const stdFirstConcept = 'avantgárd' //második sor 3. elemét eltároljuk egy változóba
+
+/**
+ * @type {string} harmadik sor 1. eleme
+ */
+const ttdAuthor = 'Thomas Mann' //harmadik sor 1. elemét eltároljuk egy váltotóba
+/**
+ * @type {string} harmadik sor 2. eleme
+ */
+const ttdCreation = 'Mario és a varázsló ' //harmadik sor 2. elemét eltároljuk egy váltotóba
+/**
+ * @type {string} harmadik sor 3. eleme
+ */
+const ttdFirstConcept = 'kisregény' //harmadik sor 3. elemét eltároljuk egy váltotóba
+
+/**
+ * @type {string} negyedik sor 1. eleme
+ */
+const fottdAuthor = 'Franz Kafka ' //negyedik és az ötödik sor 1. elemét eltároljuk egy változóba
+/**
+ * @type {string} negyedik sor 2. eleme
+ */
+const fottdCreation = 'A per' //negyedik sor 2. elemét eltároljuk egy változóba
+/**
+ * @type {string} negyedik sor 3. eleme
+ */
+const fottdFirstConcept = 'regény' //negyedik sor 3. elemét eltároljuk egy változóba
+
+/**
+ * @type {string} ötödik sor 2. eleme
+ */
+const fittdCreation = 'Az átváltozás ' //ötödik sor 2. elemét eltároljuk egy változóba
+/**
+ * @type {string} ötödik sor 3. eleme
+ */
+const fittdFirstConcept = 'kisregény' //ötödik sor 3. elemét eltároljuk egy változóba
+
+
+console.log(headAuthor,'|',headCreation,'|',headConcept,'|' ) //a változók használatával kííratjuk a headet
+console.log(ftdAuthor,'|',ftdCreation,'|',ftdFirstConcept,'|' )//a változók használatával kííratjuk az első sort
+console.log(ftdAuthor,'|',stdCreation,'|',stdFirstConcept,'|' ) //a változók segítségével kíírjuk a második sort
+console.log(ttdAuthor,'|',ttdCreation,'|',ttdFirstConcept,'|' ) //a változók segítségével kíírjuk a harmadik sort
+console.log(fottdAuthor,'|',fottdCreation,'|',fottdFirstConcept,'|' ) //a változók segítségével kíírjuk a negyedik sort
+console.log(fottdAuthor,'|',fittdCreation,'|',fittdFirstConcept,'|' ) // a változók segítségével kíírjuk az ötödik sort
