a07bea023d363efd5e4afb7de89cf81308fe88e8
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-13 22:25:38 +0100

    console row tablazat

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,6 +1,6 @@
 console.log('Szerző        | Mű                                | Fogalmak  | ')  //kíírjuk a táblázat headjet 
 console.log('Apollinaire   | A megsebzett galamb és a szökőkút | képvers   | ')  //kíírjuk a táblázat legelső adatsorát 
-console.log('|_            | Búcsú                             | avantgárd | ')  //kíírjuk a táblázat 2. adatsorát 
+console.log('Apollinaire   | Búcsú                             | avantgárd | ')  //kíírjuk a táblázat 2. adatsorát 
 console.log('Thomas Mann   | Mario és a varázsló               | kisregény | ')  //kíírjuk a táblázat 3. adatsorát 
 console.log('Franz Kafka   | A per                             | regény    | ')  //kíírjuk a táblázat 4. adatsorát 
-console.log('|_            | Az átváltozás                     | kisregény | ')  //kíírjuk a táblázat 5. adatsorát 
+console.log('Franz Kafka   | Az átváltozás                     | kisregény | ')  //kíírjuk a táblázat 5. adatsorát 
