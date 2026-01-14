0bc0fb1702a807e11186be149007211bd44f8633
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-14 20:20:12 +0100

    console row elagazas

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -31,13 +31,27 @@ const DataArray =[ //adattömb deklarálás
     },
 ]
 
-
-
-
-
 console.log(headerList[0],'|',headerList[1],'|',headerList[2],'|' ) //tömb használatával kiírjuk a fejlécet
-console.log(DataArray[0].Author,'|',DataArray[0].FirstCreation,'|',DataArray[0].FirstConcept,'|' )//a tömb használatával kííratjuk az első sort
-console.log("|_",'|',DataArray[0].SecondCreation,'|',DataArray[0].SecondConcept,'|' ) //a tömb segítségével kíírjuk a második sort
-console.log(DataArray[1].Author,'|',DataArray[1].FirstCreation,'|',DataArray[1].FirstConcept,'|' ) //a tömb segítségével kíírjuk a harmadik sort
-console.log(DataArray[2].Author,'|',DataArray[2].FirstCreation,'|',DataArray[2].FirstConcept,'|' ) //a tömb segítségével kíírjuk a negyedik sort
-console.log("|_",'|',DataArray[2].SecondCreation,'|',DataArray[2].SecondConcept,'|' ) //a tömb segítségével kíírjuk az ötödik sort
+/**
+ * @type {string} első sor adatai egy változóban, amihez majd hozzátesszük a második sort
+ */
+let FirstRowContent = DataArray[0].Author +'|'+ DataArray[0].FirstCreation+'|'+ DataArray[0].FirstConcept +'|' //az első sor adatait beletesszük egy változóba
+if(DataArray[0].SecondCreation != undefined && DataArray[0].SecondConcept != undefined ) //ellenőrizzük hogy üresek-e az objekt SecondCreation és SecondConcept mezői
+{
+    FirstRowContent += '\n'+'|_ |' + DataArray[0].SecondCreation +'|'+ DataArray[0].SecondConcept +'|' //igazág, egy új sorban hozzátesszük a maradék adatokat
+}
+console.log(FirstRowContent) //kiíratjuk az első kettő sort
+console.log(DataArray[1].Author,'|',DataArray[1].FirstCreation,'|',DataArray[1].FirstConcept,'|' ) //kiíratjuk a harmadik sort
+
+/**
+ * @type {string} negyedik sor adatai egy változóban, amihez majd hozzátesszük az ötödik sort
+ */
+let ThirdRowContent = DataArray[2].Author +'|'+ DataArray[2].FirstCreation+'|'+ DataArray[2].FirstConcept +'|' //a negyedik sor adatait beletesszük egy változóba
+if(DataArray[2].SecondCreation != undefined && DataArray[2].SecondConcept != undefined ) //ellenőrizzük hogy üresek-e az objekt SecondCreation és SecondConcept mezői
+{
+    ThirdRowContent += '\n'+'|_ |' + DataArray[2].SecondCreation +'|'+ DataArray[2].SecondConcept +'|' //igazág, egy új sorban hozzátesszük a maradék adatokat
+}
+console.log(ThirdRowContent) //kiíratjuk a negyedik, ötödik sort
+
+
+
