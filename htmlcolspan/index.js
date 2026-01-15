f738cf4451c84f6eda77fb1e6b0ed40227c8f7f7
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-15 20:03:51 +0100

    col html simple esemeny impl

--- htmlcolspan/index.js
+++ htmlcolspan/index.js
@@ -3,5 +3,37 @@
  */
 const simplebutton = document.querySelector('#simplebutton') //lekérjük a simplebuttont az idja alapján
 simplebutton.addEventListener('click',function(){ //Event a click-re
-    console.log('Sikeres a teszt') //leteszteljük, hogy jól működik-e a gomb lekérés
+
+    /**
+     * @type {HTMLTableSectionElement} a táblázat törzse
+     */
+    const tbody = document.querySelector('#tableBody') //lekérjük id alapján a táblázat törzsét
+    /**
+     * @type {HTMLTableRowElement} táblázat új sora
+     */
+    const tableRow = document.createElement('tr') //létrehozunk egy sort
+    tbody.appendChild(tableRow) //hozzáfűzzük a törzshöz
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const FirstTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(FirstTableCell) //hozzáfűzzük a sorhoz
+    FirstTableCell.innerText = 'ÚjSzerző' //belső szöveg
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const SecondTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(SecondTableCell) //hozzáfűzzük a sorhoz
+    SecondTableCell.innerText = 'ÚjMű' //belső szöveg
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const ThirdTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(ThirdTableCell) //hozzáfűzzük a sorhoz
+    ThirdTableCell.innerText = 'ÚjFogalom1' //belső szöveg
+    ThirdTableCell.colSpan = 2 //oszlopszélességet átállítjuk 2-re, mert nincsen második fogalom
+
 })
