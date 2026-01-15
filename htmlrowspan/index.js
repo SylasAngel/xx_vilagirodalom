b4937ab7eb501ae68f3fd5549c826fd72a955dc3
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-15 21:57:36 +0100

    row html ujsor

--- htmlrowspan/index.js
+++ htmlrowspan/index.js
@@ -2,6 +2,11 @@
  * @typedef {Author:string,Creation:string,FirstConcept:string,SecCreation?:string,SecConcept?:string} Author A táblázat adatai tömbjének típusa
  */
 
+    /**
+     * @type {HTMLTableSectionElement} táblázat törzse
+     */
+    const tbody = document.querySelector('#htmlTableBody') //id alapján lekérjük a táblázattörzsöt
+
 /**
  * @type {HTMLButtonElement} gomb elemet lekérjük
  */
@@ -15,37 +20,7 @@ simpleButton.addEventListener('click',function (){ //addEventListener a simple g
         Creation:'SimpleCreation', //mű
         FirstConcept:'SimpleConcept' //Fogalom
     }
-    /**
-     * @type {HTMLTableSectionElement} táblázat törzse
-     */
-    const tbody = document.querySelector('#htmlTableBody') //id alapján lekérjük a táblázattörzsöt
-    /**
-     * @type {HTMLTableRowElement} táblázat sor
-     */
-    const tableRow = document.createElement('tr') //táblázat sor elemet létrehozzuk
-    tbody.appendChild(tableRow) //hozzáfűzzük a törzshöz
-
-    /**
-     * @type {HTMLTableCellElement} táblázat cella
-     */
-    const FirstCell = document.createElement('td') //cella elem létrehozás
-    tableRow.appendChild(FirstCell) //hozzáfűzés a sorhoz
-    FirstCell.innerText = SimpleArr.Author //belső szöveg, Szerző elem
-
-    /**
-     * @type {HTMLTableCellElement} táblázat cella
-     */
-    const SecondCell = document.createElement('td') //cella elem létrehozás
-    tableRow.appendChild(SecondCell) //hozzáfűzés a sorhoz
-    SecondCell.innerText = SimpleArr.Creation //belső szöveg, Mű elem
-
-    /**
-     * @type {HTMLTableCellElement} táblázat cella
-     */
-    const ThirdCell = document.createElement('td') //cella elem létrehozás
-    tableRow.appendChild(ThirdCell) //hozzáfűzés a sorhoz
-    ThirdCell.innerText = SimpleArr.FirstConcept //belső szöveg, Fogalom elem
-
+    AddRow(SimpleArr,tbody) //meghívjuk a függvényt, hogy hozzáadja az új sort
 })
 
 /**
@@ -63,58 +38,66 @@ doubleButton.addEventListener('click',function (){ //addEventListener a simple g
         SecCreation: 'DoubleCreation2', //mű 2
         SecConcept: 'DoubleConcept2' //fogalom 2
     }
-    /**
-     * @type {HTMLTableSectionElement} táblázat törzse
-     */
-    const tbody = document.querySelector('#htmlTableBody') //id alapján lekérjük a táblázattörzsöt
-    /**
+    AddRow(DoubleArr,tbody) //meghívjuk a függvényt, hogy hozzáadja a kettő új sort
+})
+
+/**
+ * egy új sort vagy sorokat fog hozzáadni egy táblázat törzshöz
+ * @param {Author} DataArray  tömb ami tartalmazza az adatokat
+ * @param {HTMLTableSectionElement} TbodyAppend a törzs amihez hozzáfűzzük az új sort
+ * @returns {void} nem returnol semmit
+ */
+function AddRow(DataArray,TbodyAppend) //definiáljuk a függvényt
+{
+        /**
      * @type {HTMLTableRowElement} táblázat sor
      */
     const tableRow = document.createElement('tr') //táblázat sor elemet létrehozzuk
-    tbody.appendChild(tableRow) //hozzáfűzzük a törzshöz
+    TbodyAppend.appendChild(tableRow) //hozzáfűzzük a törzshöz
 
     /**
      * @type {HTMLTableCellElement} táblázat cella
      */
     const FirstCell = document.createElement('td') //cella elem létrehozás
     tableRow.appendChild(FirstCell) //hozzáfűzés a sorhoz
-    FirstCell.innerText = DoubleArr.Author //belső szöveg, Szerző elem
+    FirstCell.innerText = DataArray.Author //belső szöveg, Szerző elem
 
     /**
      * @type {HTMLTableCellElement} táblázat cella
      */
     const SecondCell = document.createElement('td') //cella elem létrehozás
     tableRow.appendChild(SecondCell) //hozzáfűzés a sorhoz
-    SecondCell.innerText = DoubleArr.Creation //belső szöveg, Mű elem
+    SecondCell.innerText = DataArray.Creation //belső szöveg, Mű elem
 
     /**
      * @type {HTMLTableCellElement} táblázat cella
      */
     const ThirdCell = document.createElement('td') //cella elem létrehozás
     tableRow.appendChild(ThirdCell) //hozzáfűzés a sorhoz
-    ThirdCell.innerText = DoubleArr.FirstConcept //belső szöveg, Fogalom elem
+    ThirdCell.innerText = DataArray.FirstConcept //belső szöveg, Fogalom elem
 
+    if(DataArray.SecConcept != undefined && DataArray.SecCreation != undefined) //igazág megnézzük hogy a második mű és fogalom adatok az üresek-e
+    {
     /**
      * @type {HTMLTableRowElement} 2.sor
      */
     const SecondRow = document.createElement('tr') //létrehozunk egy 2. sort
-    tbody.appendChild(SecondRow) //hozzáfűzzük a törszhöz
+    TbodyAppend.appendChild(SecondRow) //hozzáfűzzük a törszhöz
 
     /**
      * @type {HTMLTableCellElement} cella
      */
     const FourthCell = document.createElement('td') //cella elem létrehozás
     SecondRow.appendChild(FourthCell) //hozzáfűzés a sorhoz
-    FourthCell.innerText = DoubleArr.SecCreation //belső szöveg, 2. mű elem
+    FourthCell.innerText = DataArray.SecCreation //belső szöveg, 2. mű elem
 
     /**
      * @type {HTMLTableCellElement} cella
      */
     const FifthCell = document.createElement('td') //cella elem létrehozás
     SecondRow.appendChild(FifthCell) //hozzáfűzés a sorhoz
-    FifthCell.innerText = DoubleArr.SecConcept //belső szöveg, 2. Fogalom elem
+    FifthCell.innerText = DataArray.SecConcept //belső szöveg, 2. Fogalom elem
 
     FirstCell.rowSpan = 2 //sorszélességet átállítjuk 2-re , mert ez duplasoros
-
-
-})
+    }
+}
