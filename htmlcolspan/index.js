a16e7e3b528700d9d5a83a6dd97b831411317ab5
rabadon Veigar <vas.zoltan_2022b@bolyai-szakkozep.hu>
2026-01-15 20:07:39 +0100

    col html double esemeny impl

--- htmlcolspan/index.js
+++ htmlcolspan/index.js
@@ -37,3 +37,52 @@ simplebutton.addEventListener('click',function(){ //Event a click-re
     ThirdTableCell.colSpan = 2 //oszlopszélességet átállítjuk 2-re, mert nincsen második fogalom
 
 })
+
+/**
+ * @type {HTMLButtonElement} doublebutton
+ */
+const doublebutton = document.querySelector('#doublebutton') //lekérjük a doublebuttont az idja alapján
+doublebutton.addEventListener('click',function(){ //Event a click-re
+
+    /**
+     * @type {HTMLTableSectionElement} a táblázat törzse
+     */
+    const tbody = document.querySelector('#tableBody') //lekérjük id alapján a táblázat törzsét
+    /**
+     * @type {HTMLTableRowElement} táblázat új sora
+     */
+    const tableRow = document.createElement('tr') //létrehozunk egy sort
+    tbody.appendChild(tableRow) //hozzáfűzzük a törzshöz
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const FirstTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(FirstTableCell) //hozzáfűzzük a sorhoz
+    FirstTableCell.innerText = 'ÚjDoubleSzerző' //belső szöveg
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const SecondTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(SecondTableCell) //hozzáfűzzük a sorhoz
+    SecondTableCell.innerText = 'ÚjDoubleMű' //belső szöveg
+
+    /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const ThirdTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(ThirdTableCell) //hozzáfűzzük a sorhoz
+    ThirdTableCell.innerText = 'ÚjDoubleFogalom1' //belső szöveg
+
+        /**
+     * @type {HTMLTableCellElement} új cella
+     */
+    const FourthTableCell = document.createElement('td') //létrehozunk egy cellát
+    tableRow.appendChild(FourthTableCell) //hozzáfűzzük a sorhoz
+    FourthTableCell.innerText = 'ÚjDoubleFogalom2' //belső szöveg
+
+
+
+
+})
